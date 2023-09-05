2f56aaa9838f288801d3648f8329219b1daf8ad7
User <User@WIN-A9GL8ECUKAH>
2023-09-05 21:02:57 +0300

    Добавьте файлы проекта.

---
+++ WpfApp3/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //ColumnDefinition columnDefinition1 = new ColumnDefinition();
+            //ColumnDefinition columnDefinition2 = new ColumnDefinition();
+            //ColumnDefinition columnDefinition3 = new ColumnDefinition();
+            //grid1.ColumnDefinitions.Add(columnDefinition1);
+            //grid1.ColumnDefinitions.Add(columnDefinition2);
+            //grid1.ColumnDefinitions.Add(columnDefinition3);
+            //RowDefinition rowDefinition1 = new RowDefinition();
+            //RowDefinition rowDefinition3 = new RowDefinition();
+            //RowDefinition rowDefinition2 = new RowDefinition();
+            //grid1.RowDefinitions.Add(rowDefinition1);
+            //grid1.RowDefinitions.Add(rowDefinition2);
+            //grid1.RowDefinitions.Add(rowDefinition3);
+            //Button button1 = new Button() { Content = "1" };
+            //Button button2 = new Button() { Content = "2" };
+            //Button button3 = new Button() { Content = "3" };
+            //Button button4 = new Button() { Content = "4" };
+            //Button button5 = new Button() { Content = "5" };
+            //grid1.Children.Add(button1);
+            //Grid.SetColumnSpan(button1, 2);
+            //Grid.SetColumn(button1, 0);
+            //Grid.SetRow(button1, 0);
+            //grid1.Children.Add(button2);
+            //Grid.SetRowSpan(button2, 2);
+            //Grid.SetColumn(button2, 3);
+            //Grid.SetRow(button2, 0);
+            //grid1.Children.Add(button3);
+            //Grid.SetColumn(button3, 0);
+            //Grid.SetRow(button3, 1);
+            //grid1.Children.Add(button4);
+            //Grid.SetColumn(button4, 1);
+            //Grid.SetRow(button4, 1);
+            //grid1.Children.Add(button5);
+            //Grid.SetColumnSpan(button5, 3);
+            //Grid.SetColumn(button5, 0);
+            //Grid.SetRow(button5, 2);
+
+        }
+    }
+}
